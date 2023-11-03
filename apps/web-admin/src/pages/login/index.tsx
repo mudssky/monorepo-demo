84c0e9c90141ac13f5d92fae0a76d8c584149b7c
mudssky <mudssky@gmail.com>
2023-11-03 23:55:45 +0800

    ✨ feat: update tailwind config

--- apps/web-admin/src/pages/login/index.tsx
+++ apps/web-admin/src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Checkbox, Form, Input } from 'antd'
-
+import styles from './styles.module.css'
 const onFinish = (values: any) => {
   console.log('Success:', values)
 }
@@ -15,51 +15,57 @@ type FieldType = {
 }
 export function Login() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-blue-300">
-      <div className="flex h-[400px] w-1/4 flex-col items-center justify-center rounded-lg bg-slate-100">
-        <div className="mb-4 flex justify-center text-3xl">
+    <div className={styles['login-container']}>
+      <div className="card glass w-96">
+        <div className="card-title justify-center">
           <span>系统登陆</span>
         </div>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item<FieldType>
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+        <div className="card-body">
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="用户名"
+              name="username"
+              rules={[
+                { required: true, message: 'Please input your username!' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password!' },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 8, span: 16 }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </div>
   )
