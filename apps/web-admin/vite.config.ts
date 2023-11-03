86a6d3568fff8c10c89bedfbd8b17ce63a76b44f
mudssky <mudssky@gmail.com>
2023-11-03 09:44:51 +0800

    🐳 chore: config server

--- apps/web-admin/vite.config.ts
+++ apps/web-admin/vite.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT),
+      proxy: {
+        '/api': {
+          target: env.VITE_PROXY_TARGET, //开发环境
+          changeOrigin: true,
+          rewrite: (path) => {
+            return path
+          },
+        },
+        '/static': {
+          target: env.VITE_PROXY_TARGET, //开发环境
+          changeOrigin: true,
+        },
+      },
+    },
+    plugins: [react()],
+  }
 })
