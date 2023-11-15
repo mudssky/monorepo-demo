2c057bc0c462870e63f79664ade6e3696796e0a6
mudssky <mudssky@gmail.com>
2023-11-15 23:44:05 +0800

    ✨ feat: update southplus bookmark

--- apps/jsbookmark/src/scripts/deprecated/southplus-JumpBuy.ts
+++ apps/jsbookmark/src/scripts/deprecated/southplus-JumpBuy.ts
@@ -1,6 +1,11 @@
 export const jumpBuyArea = () => {
   const buyArea = document.querySelector('.quote.jumbotron') as HTMLDivElement
-  buyArea.scrollIntoView({ behavior: 'smooth', block: 'start' })
+  // 立刻执行，如果换成smooth，接下来的scrollTo会覆盖
+  buyArea.scrollIntoView({ behavior: 'instant', block: 'start' })
+  window.scrollTo({
+    top: window.scrollY - 100,
+    behavior: 'smooth',
+  })
   return buyArea
 }
 jumpBuyArea()
