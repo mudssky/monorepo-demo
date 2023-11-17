3128a478bcae055a270f1bb515800fb371b02b0a
mudssky <mudssky@gmail.com>
2023-11-17 21:07:16 +0800

    🎈 perf: swc jest config

--- apps/server/src/modules/health/health.controller.spec.ts
+++ apps/server/src/modules/health/health.controller.spec.ts
@@ -1,18 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HealthController } from './health.controller';
+import { HttpModule } from '@nestjs/axios'
+import { TerminusModule } from '@nestjs/terminus'
+import { Test, TestingModule } from '@nestjs/testing'
+import { TerminusLogger } from '../logger/terminus-logger.service'
+import { MockGlobalModule } from '../mock-global/mock-global.module'
+import { HealthController } from './health.controller'
 
 describe('HealthController', () => {
-  let controller: HealthController;
+  let controller: HealthController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MockGlobalModule,
+        TerminusModule.forRoot({
+          // 自定义logger，和全局logger继承
+          logger: TerminusLogger,
+        }),
+        HttpModule,
+      ],
       controllers: [HealthController],
-    }).compile();
+      // providers: [HealthCheckService],
+    }).compile()
 
-    controller = module.get<HealthController>(HealthController);
-  });
+    controller = module.get<HealthController>(HealthController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
