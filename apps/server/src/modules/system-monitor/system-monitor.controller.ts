0d1437da0374100100049c8963eb30488cca07e6
mudssky <mudssky@gmail.com>
2023-11-19 00:19:47 +0800

    🔧 build: update server deps

--- apps/server/src/modules/system-monitor/system-monitor.controller.ts
+++ apps/server/src/modules/system-monitor/system-monitor.controller.ts
@@ -1,6 +1,7 @@
+import { ApiCustomResponse } from '@/common/decorators/swagger'
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager'
 import { Controller, Get, UseInterceptors } from '@nestjs/common'
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { StaticDataDto } from './dto/system-monitor.dto'
 import { SystemMonitorService } from './system-monitor.service'
 
@@ -9,7 +10,7 @@ import { SystemMonitorService } from './system-monitor.service'
 export class SystemMonitorController {
   constructor(private readonly systemMonitorService: SystemMonitorService) {}
   @ApiOperation({ summary: '获取硬件信息' })
-  @ApiResponse({
+  @ApiCustomResponse({
     type: StaticDataDto,
   })
   // @CacheKey('custom_key') 也可以覆盖缓存的key值
