1c78b2cc727855136ab336ec2f6d2056bc730868
mudssky <mudssky@gmail.com>
2023-12-25 18:06:10 +0800

    ✨ feat: update deps

---
+++ apps/web-admin/src/utils/areadata.ts
@@ -0,0 +1,13 @@
+import areadataJson from "@/data/areadata.json"
+
+
+// 省市，已知省市寻找区
+export function findArea(province: string, city: string) {
+	console.log({areadataJson});
+	
+	const provinceObj = areadataJson.find((item) =>  item.name === province )
+	const cityObj = provinceObj?.children?.find((item) =>  item.name === city )
+	const area= cityObj?.children?.[0]?.name??''
+	// console.log({ provinceObj, cityObj ,area});
+	return area
+}
