110846e9e84b4967a8acda2729e7888e02582e1b
mudssky <mudssky@gmail.com>
2024-01-10 10:33:43 +0800

    ✨ feat: update LoggerMiddleware

--- apps/server/src/common/middlewares/logger/logger.middleware.ts
+++ apps/server/src/common/middlewares/logger/logger.middleware.ts
@@ -1,6 +1,6 @@
 import { GlobalLoggerService } from '@/modules/logger/logger.service'
 import { Injectable, NestMiddleware } from '@nestjs/common'
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly logger: GlobalLoggerService) {}
@@ -8,6 +8,22 @@ export class LoggerMiddleware implements NestMiddleware {
     const dayjs = (await import('dayjs')).default
     // console.log({ dayjs })
     const start = dayjs()
+
+    next()
+
+    // 请求接收后打印信息,放在next（）之后是因为要等一些中间件执行完，才能获得路径等信息
+    this.logger.debug({
+      label: 'enter router',
+      method: req.method,
+      path: req.path,
+      url: req.url,
+      ip: req.ip,
+      query: req.query,
+      userAgent: req.get('User-Agent'),
+      body: req.body,
+    })
+
+    // 响应结束后打印信息
     res.on('finish', () => {
       const cost = dayjs().diff(start, 'milliseconds')
       this.logger.debug({
@@ -24,6 +40,5 @@ export class LoggerMiddleware implements NestMiddleware {
         cost,
       })
     })
-    next()
   }
 }
