b549bc9737b3b58ebc9c125f095100b848a9079e
mudssky <mudssky@gmail.com>
2024-01-10 19:21:06 +0800

    ✨ feat: config static server

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -41,7 +41,16 @@ import { FileModule } from './modules/upload-file/upload-file.module'
     // 静态文件路径配置
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, '..', 'static'),
-      // renderPath: '/static',
+      //服务器根路径配置
+      serveRoot: '/static',
+      serveStaticOptions: {
+        // 缓存控制
+        cacheControl: true,
+        // 设为true后碰到目录会查找目录下的index.html
+        index: true,
+        // 尾部是/的时候重定向
+        redirect: true,
+      },
     }),
     CustomCacheModule,
     PrismaModule,
