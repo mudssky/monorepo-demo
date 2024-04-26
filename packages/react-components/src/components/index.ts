73147da0450a6b5647b809cd05ba9fb005893d9b
mudssky <mudssky@gmail.com>
2024-04-26 10:10:58 +0800

    ✨ feat: add Space components

--- packages/react-components/src/components/index.ts
+++ packages/react-components/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Calendar/index'
 export { default as Calendar } from './Calendar/index'
+export { default as Space } from './Space/index'
---
+++ packages/react-components/src/stories/components/Space/Space.stories.tsx
@@ -0,0 +1,40 @@
+import { Space } from '@mudssky/react-components'
+import type { Meta, StoryObj } from '@storybook/react'
+import './styles.scss'
+
+const meta = {
+  title: 'Space',
+  component: Space,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    // value: {
+    //   //   control: 'date',
+    // },
+  },
+} satisfies Meta<typeof Space>
+
+type Story = StoryObj<typeof meta>
+export default meta
+
+export const Primary: Story = {
+  args: {
+    // children: [<div>111</div>, <div>222</div>, <div>333</div>],
+    direction: 'horizontal',
+    align: 'end',
+    wrap: true,
+    size: ['large', 'small'],
+    className: 'container',
+  },
+  render(args) {
+    return (
+      <Space {...args}>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </Space>
+    )
+  },
+}
