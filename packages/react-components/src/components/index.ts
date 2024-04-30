0ed261e0b4c28f69836e13497b614f3183efcba3
mudssky <mudssky@gmail.com>
2024-04-30 09:16:29 +0800

    ✨ feat: add message components

--- packages/react-components/src/components/index.ts
+++ packages/react-components/src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './Calendar/index'
 export { default as Calendar } from './Calendar/index'
+export * from './Message/index'
 export { default as Portal } from './Portal/index'
 export { default as Space } from './Space/index'
---
+++ packages/react-components/src/stories/components/Message/Message.stories.tsx
@@ -0,0 +1,45 @@
+import { MessageProvider, MessageRef } from '@mudssky/react-components'
+import type { Meta, StoryObj } from '@storybook/react'
+import { useRef } from 'react'
+
+const meta = {
+  title: 'MessageProvider',
+  component: MessageProvider,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    // value: {
+    //   //   control: 'date',
+    // },
+  },
+} satisfies Meta<typeof MessageProvider>
+
+type Story = StoryObj<typeof meta>
+export default meta
+
+export const Primary: Story = {
+  args: {
+    // content: '水印',
+    // content: ['测试水印', '哦哦'],
+  },
+  render(args) {
+    const messageRef = useRef<MessageRef>(null)
+    return (
+      <div>
+        <button
+          onClick={() => {
+            messageRef.current?.add({
+              content: '请求成功',
+              duration: 2000,
+            })
+          }}
+        >
+          成功
+        </button>
+        <MessageProvider ref={messageRef} {...args}></MessageProvider>
+      </div>
+    )
+  },
+}
