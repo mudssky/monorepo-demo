f01643dcea23dbd71438c2842e48d3e83d618a46
mudssky <mudssky@gmail.com>
2024-07-31 18:55:40 +0800

    ✨ feat: add iframe preview

---
+++ apps/react-playground/src/components/Preview/index.tsx
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from 'react'
+import { PlaygroundContext } from '../ReactPlayground/PlaygroundContext'
+import { compile } from './complie'
+import { IMPORT_MAP_FILE_NAME } from '../ReactPlayground/files'
+import iframeRaw from './iframe.html?raw'
+
+export default function Preview() {
+  const { files } = useContext(PlaygroundContext)
+  const [compiledCode, setCompiledCode] = useState('')
+
+  const getIframeUrl = () => {
+    const res = iframeRaw
+      .replace(
+        '<script type="importmap"></script>',
+        `<script type="importmap">${
+          files[IMPORT_MAP_FILE_NAME].value
+        }</script>`,
+      )
+      .replace(
+        '<script type="module" id="appSrc"></script>',
+        `<script type="module" id="appSrc">${compiledCode}</script>`,
+      )
+    return URL.createObjectURL(new Blob([res], { type: 'text/html' }))
+  }
+
+  const [iframeUrl, setIframeUrl] = useState(getIframeUrl())
+
+  useEffect(() => {
+    const res = compile(files)
+    setCompiledCode(res)
+  }, [files])
+
+  useEffect(() => {
+    setIframeUrl(getIframeUrl())
+  }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode])
+
+  return (
+    <div style={{ height: '100%' }}>
+      <iframe
+        src={iframeUrl}
+        style={{
+          width: '100%',
+          height: '100%',
+          padding: 0,
+          border: 'none',
+        }}
+      />
+      {/* <Editor file={{
+            name: 'dist.js',
+            value: compiledCode,
+            language: 'javascript'
+        }}/> */}
+    </div>
+  )
+}
--- apps/react-playground/src/components/ReactPlayground/index.tsx
+++ apps/react-playground/src/components/ReactPlayground/index.tsx
@@ -2,6 +2,7 @@ import { Allotment } from 'allotment'
 import 'allotment/dist/style.css'
 import Header from '@/components/Header'
 import CodeEditor from '../CodeEditor'
+import Preview from '@/components/Preview'
 
 export default function ReactPlayground() {
   return (
@@ -12,7 +13,7 @@ export default function ReactPlayground() {
           <CodeEditor />
         </Allotment.Pane>
         <Allotment.Pane minSize={0}>
-          <div>222</div>
+          <Preview></Preview>
         </Allotment.Pane>
       </Allotment>
     </div>
