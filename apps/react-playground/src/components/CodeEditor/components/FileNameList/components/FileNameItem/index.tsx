c43ffcd65ef37a92debe6b5310a74b42d0d039e9
mudssky <mudssky@gmail.com>
2024-08-01 09:40:10 +0800

    ✨ feat: file crud

--- apps/react-playground/src/components/CodeEditor/components/FileNameList/components/FileNameItem/index.tsx
+++ apps/react-playground/src/components/CodeEditor/components/FileNameList/components/FileNameItem/index.tsx
@@ -2,24 +2,86 @@ import clsx from 'clsx'
 import React, { useState, useRef, useEffect } from 'react'
 
 import styles from './index.module.scss'
+import { Popconfirm } from 'antd'
 
 export interface FileNameItemProps {
   value: string
   actived: boolean
+  creating?: boolean
+  readonly: boolean
+  onRemove: () => void
   onClick: () => void
+  onEditComplete: (name: string, prevName: string) => void
 }
 
 export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
-  const { value, actived = false, onClick } = props
-
+  const {
+    creating,
+    value,
+    actived = false,
+    readonly,
+    onClick,
+    onEditComplete,
+    onRemove,
+  } = props
+  const [editing, setEditing] = useState(creating)
+  const inputRef = useRef<HTMLInputElement>(null)
   const [name, setName] = useState(value)
+  const handleDoubleClick = () => {
+    setEditing(true)
+    setTimeout(() => {
+      inputRef?.current?.focus()
+    }, 0)
+  }
+  const handleInputBlur = () => {
+    setEditing(false)
+    onEditComplete?.(name, value)
+  }
+
+  useEffect(() => {
+    if (creating) {
+      inputRef?.current?.focus()
+    }
+  }, [creating])
 
   return (
     <div
       className={clsx(styles['tab-item'], actived ? styles.actived : null)}
       onClick={onClick}
     >
-      <span>{name}</span>
+      {editing ? (
+        <input
+          ref={inputRef}
+          className={styles['tabs-item-input']}
+          value={name}
+          onBlur={handleInputBlur}
+          onChange={(e) => setName(e.target.value)}
+        />
+      ) : (
+        <>
+          <span onDoubleClick={!readonly ? handleDoubleClick : () => {}}>
+            {name}
+          </span>
+          {!readonly ? (
+            <Popconfirm
+              title="确认删除该文件吗？"
+              okText="确定"
+              cancelText="取消"
+              onConfirm={(e) => {
+                e?.stopPropagation()
+                onRemove()
+              }}
+            >
+              <span style={{ marginLeft: 5, display: 'flex' }}>
+                <svg width="12" height="12" viewBox="0 0 24 24">
+                  <line stroke="#999" x1="18" y1="6" x2="6" y2="18"></line>
+                  <line stroke="#999" x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </span>
+            </Popconfirm>
+          ) : null}
+        </>
+      )}
     </div>
   )
 }
--- apps/react-playground/src/components/CodeEditor/components/FileNameList/index.tsx
+++ apps/react-playground/src/components/CodeEditor/components/FileNameList/index.tsx
@@ -2,6 +2,11 @@ import { PlaygroundContext } from '@/components/ReactPlayground/PlaygroundContex
 import { useContext, useEffect, useState } from 'react'
 import { FileNameItem } from './components/FileNameItem'
 import styles from './components/FileNameItem/index.module.scss'
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+} from '@/components/ReactPlayground/files'
 export default function FileNameList() {
   const {
     files,
@@ -12,21 +17,53 @@ export default function FileNameList() {
     setSelectedFileName,
   } = useContext(PlaygroundContext)
   const [tabs, setTabs] = useState([''])
+  const [creating, setCreating] = useState(false)
 
   useEffect(() => {
     setTabs(Object.keys(files))
   }, [files])
 
+  const handleEditComplete = (name: string, prevName: string) => {
+    updateFileName(prevName, name)
+    setSelectedFileName(name)
+    setCreating(false)
+  }
+
+  const addTab = () => {
+    const newFileName = 'Comp' + Math.random().toString().slice(2, 8) + '.tsx'
+    addFile(newFileName)
+    setSelectedFileName(newFileName)
+    setCreating(true)
+  }
+
+  const handleRemove = (name: string) => {
+    removeFile(name)
+    setSelectedFileName(ENTRY_FILE_NAME)
+  }
+  const readonlyFilenames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ]
   return (
     <div className={styles.tabs}>
       {tabs.map((item, index) => (
         <FileNameItem
+          creating={creating}
+          readonly={readonlyFilenames.includes(item)}
           key={item + index}
           onClick={() => setSelectedFileName(item)}
           actived={selectedFileName === item}
           value={item}
+          onEditComplete={handleEditComplete}
+          onRemove={() => {
+            handleRemove(item)
+          }}
         ></FileNameItem>
       ))}
+      <div className={styles.add} onClick={addTab}>
+        +
+      </div>
     </div>
   )
 }
