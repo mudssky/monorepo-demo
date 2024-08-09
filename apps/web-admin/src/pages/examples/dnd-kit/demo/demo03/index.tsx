8c50558cf8463e53a86e5e667f24be1ac0ec2725
mudssky <mudssky@gmail.com>
2024-08-09 17:27:07 +0800

    ✨ feat: add dnd kit demos

---
+++ apps/web-admin/src/pages/examples/dnd-kit/demo/demo03/index.tsx
@@ -0,0 +1,95 @@
+import {
+  closestCenter,
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import type { DragEndEvent } from '@dnd-kit/core/dist/types/index'
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  SortableContext,
+  useSortable,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { Flex, Tag } from 'antd'
+import React, { useState } from 'react'
+
+interface Item {
+  id: number
+  text: string
+}
+
+interface DraggableTagProps {
+  tag: Item
+}
+
+const commonStyle: React.CSSProperties = {
+  cursor: 'move',
+  transition: 'unset', // Prevent element from shaking after drag
+}
+
+const DraggableTag: React.FC<DraggableTagProps> = (props) => {
+  const { tag } = props
+  const { listeners, transform, transition, isDragging, setNodeRef } =
+    useSortable({ id: tag.id })
+
+  const style = transform
+    ? {
+        ...commonStyle,
+        // transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+        transform: CSS.Translate.toString(transform),
+        transition: isDragging ? 'unset' : transition, // Improve performance/visual effect when dragging
+      }
+    : commonStyle
+
+  return (
+    <Tag style={style} ref={setNodeRef} {...listeners}>
+      {tag.text}
+    </Tag>
+  )
+}
+
+const Demo03: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([
+    { id: 1, text: 'Tag 1' },
+    { id: 2, text: 'Tag 2' },
+    { id: 3, text: 'Tag 3' },
+  ])
+
+  const sensors = useSensors(useSensor(PointerSensor))
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (!over) {
+      return
+    }
+    if (active.id !== over.id) {
+      setItems((data) => {
+        const oldIndex = data.findIndex((item) => item.id === active.id)
+        const newIndex = data.findIndex((item) => item.id === over.id)
+        return arrayMove(data, oldIndex, newIndex)
+      })
+    }
+  }
+
+  return (
+    <DndContext
+      sensors={sensors}
+      onDragEnd={handleDragEnd}
+      //   从矩形数组中返回里指定矩形最近的矩形
+      collisionDetection={closestCenter}
+    >
+      <SortableContext items={items} strategy={horizontalListSortingStrategy}>
+        <Flex gap="4px 0" wrap>
+          {items.map<React.ReactNode>((item) => (
+            <DraggableTag tag={item} key={item.id} />
+          ))}
+        </Flex>
+      </SortableContext>
+    </DndContext>
+  )
+}
+
+export default Demo03
--- apps/web-admin/src/pages/examples/dnd-kit/index.tsx
+++ apps/web-admin/src/pages/examples/dnd-kit/index.tsx
@@ -1,6 +1,9 @@
 import { Tabs, TabsProps } from 'antd'
 import Demo01 from './demo/demo01/index'
 import Demo02 from './demo/demo02/index'
+import Demo03 from './demo/demo03'
+import Demo04 from './demo/demo04'
+import Demo05 from './demo/demo05'
 
 const items: TabsProps['items'] = [
   {
@@ -12,6 +15,21 @@ const items: TabsProps['items'] = [
     key: '2',
     label: 'quick start2',
     children: <Demo02 />,
+  },
+  {
+    key: '3',
+    label: 'tags',
+    children: <Demo03 />,
+  },
+  {
+    key: '4',
+    label: 'table-row',
+    children: <Demo04 />,
+  },
+  {
+    key: '5',
+    label: 'table-column',
+    children: <Demo05 />,
   },
 ]
 
