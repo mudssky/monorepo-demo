69a7604eb99b9d64da5d5acf9f2c921586479223
mudssky <mudssky@gmail.com>
2025-08-09 22:02:05 +0800

    refactor(eslint): 迁移到新的ESLint扁平化配置
    删除旧的.eslintrc.cjs文件，新增eslint.config.js使用扁平化配置
    更新package.json中的相关依赖

---
+++ apps/web-admin/eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      // 启用 react-hooks 插件的推荐规则
+      ...pluginReactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['*.cjs'],
+  },
+])
