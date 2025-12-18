3b5c28031c5800c49d0558ad45ceaab1aa0c786b
mudssky <mudssky@gmail.com>
2025-12-18 09:56:59 +0800

    build(eslint): 添加tsconfig.eslint.json并更新eslint配置
    添加专用的tsconfig.eslint.json文件以支持ESLint的类型检查
    更新eslint.config.js以指定TypeScript解析配置

--- apps/server/eslint.config.js
+++ apps/server/eslint.config.js
@@ -1,3 +1,14 @@
 const nestConfig = require('@mudssky/eslint-config-custom/nest')
 
-module.exports = [{ ignores: ['dist'] }, ...nestConfig]
+module.exports = [
+  { ignores: ['dist'] },
+  ...nestConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+]
