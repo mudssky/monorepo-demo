a50425b7bc11244a7575227bc600c4520bfcb497
mudssky <mudssky@gmail.com>
2025-12-19 02:00:19 +0800

    build(webpack): 添加tsconfig-paths-webpack-plugin并更新配置
    使用tsconfig-paths-webpack-plugin替代手动配置alias，简化路径解析
    移除手动alias配置，改为通过tsconfig.json统一管理路径
    添加webpack-node-externals以优化打包体积

--- apps/server/webpack.config.js
+++ apps/server/webpack.config.js
@@ -2,23 +2,41 @@ const swcDefaultConfig =
   require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory()
     .swcOptions
 const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+import nodeExternals from 'webpack-node-externals'
 
 module.exports = {
   devtool: 'source-map',
   resolve: {
-    extensions: ['.ts', '.js', '.mjs'],
+    extensions: ['.ts', '.js', '.mjs', '.json'],
     extensionAlias: {
       '.js': ['.ts', '.js'],
       '.mjs': ['.mts', '.mjs'],
     },
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@app/redis': path.resolve(__dirname, 'libs/redis/src'),
-      '@lib': path.resolve(__dirname, 'libs/index'),
-      '@root': path.resolve(__dirname),
-      '#prisma/client': path.resolve(__dirname, 'prisma/generated/client'),
-    },
+    plugins: [
+      new TsconfigPathsPlugin({
+        // 指定 tsconfig.json 文件的位置（可选，默认会查找项目根目录）
+        configFile: path.resolve(__dirname, './tsconfig.json'),
+      }),
+    ],
+    // alias: {
+    //   '@': path.resolve(__dirname, 'src'),
+    //   '@app/redis': path.resolve(__dirname, 'libs/redis/src'),
+    //   '@lib': path.resolve(__dirname, 'libs/index'),
+    //   '@root': path.resolve(__dirname),
+    //   '#prisma/client': path.resolve(__dirname, 'prisma/generated/client'),
+    // },
   },
+  externals: [
+    nodeExternals({
+      allowlist: [
+        'webpack/hot/poll?100',
+        '@monorepo-demo/logger',
+        '@monorepo-demo/redis',
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
